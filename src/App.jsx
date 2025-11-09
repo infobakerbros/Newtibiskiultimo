98f875f143bab831ca98b6e92b4f31f86cc97a6a
infobakerbros <info.bakerbros@gmail.com>
2025-11-08 21:53:18 -0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="p-10 text-center">
+      <h1 className="text-4xl font-bold text-yellow-400">
+        Tibiski Portal Listo
+      </h1>
+      <p className="mt-4 text-gray-200">
+        Proyecto listo para subir a GitHub o Vercel 🚀
+      </p>
+    </div>
+  );
+}
