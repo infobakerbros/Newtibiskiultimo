7237284d1ca1fb02426bf0eb4a5e910484ed4aaf
infobakerbros <info.bakerbros@gmail.com>
2025-11-08 21:51:01 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        tibiskiGreen: "#0d4c38",
+        tibiskiGold: "#f0c243",
+        tibiskiBeige: "#e9ddc9",
+      }
+    },
+  },
+  plugins: [],
+}
